2cb4ec138418cb3fd93314b4f8c72c3ed2fcbe8f
Farid Efendi <miftahfaridse@gmail.com>
2019-03-16 09:54:36 +0700

    Basic foundation of the system

---
+++ application/models/Invoice_model.php
@@ -0,0 +1,207 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Invoice_model extends CI_Model {
+
+    const STATUS_PAID = 1;
+    const STATUS_UNPAID = 0;
+    const STATUS_REFUND = 2;
+
+    const TYPE_ORDER = 'order';
+
+    function __construct()
+    {
+        parent::__construct();
+        /*cache control*/
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        $this->output->set_header('Pragma: no-cache');
+    }
+
+    public function get_invoice($id = 0) {
+        if ($id > 0) {
+            $this->db->where('id', $id);
+        }
+
+        return $this->db->get('invoice');
+    }
+
+    public function get_all_invoice($id = 0) {
+        $this->db->select('invoice.*, users.first_name, users.last_name');
+        if ($id > 0) {
+            $this->db->where('id', $id);
+        }
+        $this->db->join('users', 'invoice.user_id = users.id', 'left');
+
+        return $this->db->get('invoice');
+    }
+
+    public function get_invoices_by_status($status = 0) {
+        $this->db->select('invoice.*, users.first_name, users.last_name');
+        $this->db->where('status', $status);
+
+        $this->db->join('users', 'invoice.user_id = users.id', 'left');
+
+        return $this->db->get('invoice');
+    }
+
+    public function add_invoice($data = []) {
+        $data0['user_id'] = $this->session->userdata('user_id');
+        $data0['serie'] = $this->getInvoiceNumber(self::STATUS_UNPAID, 'serie');
+        $data0['nr'] = $this->getInvoiceNumber(self::STATUS_UNPAID, 'nr');
+        $data0['cash'] = $this->input->post('total_price_of_checking_out');
+        $data0['currency_id'] = $data['currency_id'];
+        $data0['change_value'] = $data['change_value'];
+        $data0['status'] = 1;
+        $data0['date_added'] = strtotime(date("Y-m-d H:i:s"));
+        $data0['last_modified'] = strtotime(date("Y-m-d H:i:s"));
+
+        try {
+            $this->db->insert('invoice', $data0);
+            $invoice_id = $this->db->insert_id();
+        } catch (Exception $e) {}
+
+        if ($invoice_id > 0) {
+            $group_id = $this->getOrderNextGroupId();
+            $no = 1;
+            foreach ($data['cart_items'] as $course_id => $course) {
+                $data1 = [];
+                $data1['user_id'] = $data0['user_id'];
+                $data1['course_id'] = $course_id;
+                $data1['group_id'] = $group_id;
+                $data1['group_master'] = ($no == 1)? 1 : 0;
+                $data1['title'] = $course['title'];
+                $data1['invoice_id'] = $invoice_id;
+                $data1['quantity'] = 1;
+                $data1['price'] = (!empty($course['discounted_price']))? $course['discounted_price'] : $course['price'];
+                $data1['discount'] = (!empty($course['discounted_price']))? ($course['price'] - $course['discounted_price']) : 0;
+                $data1['currency_id'] = $data['currency_id'];
+                $data1['change_value'] = $data['change_value'];
+                $data1['type'] = 1;
+                $data1['status'] = 0;
+                $data1['date_added'] = strtotime(date("Y-m-d H:i:s"));
+                $data1['last_modified'] = strtotime(date("Y-m-d H:i:s"));
+                $this->db->insert('orders', $data1);
+                $order_id = $this->db->insert_id();
+                if ($order_id) {
+                    // invoice item
+                    $data2 = [];
+                    $data2['invoice_id'] = $invoice_id;
+                    $data2['type'] = self::TYPE_ORDER;
+                    $data2['rel_id'] = $order_id;
+                    $data2['title'] = $course['title'];
+                    $data2['quantity'] = 1;
+                    $data2['price'] = (!empty($course['discounted_price']))? $course['discounted_price'] : $course['price'];
+                    $data2['date_added'] = strtotime(date("Y-m-d H:i:s"));
+                    $data2['last_modified'] = strtotime(date("Y-m-d H:i:s"));
+
+                    $this->db->insert('invoice_item', $data2);
+                    $invoice_item_id = $this->db->insert_id();
+                }
+
+                $no++;
+            }
+        }
+        $this->session->set_flashdata('flash_message', get_phrase('order_added_successfully'));
+    }
+
+    public function edit_invoice($invoice_id = "") { // Admin does this editing
+        $data = [];
+        $this->db->where('id', $invoice_id);
+        $this->db->update('invoice', $data);
+        $this->session->set_flashdata('flash_message', get_phrase('invoice_update_successfully'));
+    }
+
+    public function delete_invoice($invoice_id = "") {
+        $this->db->where('id', $invoice_id);
+        $this->db->delete('invoice');
+        $this->session->set_flashdata('flash_message', get_phrase('invoice_deleted_successfully'));
+    }
+
+    public function getInvoiceNumber($status, $type)
+    {
+        if ($status == self::STATUS_PAID)
+            $serie = 'PAID-';
+        elseif ($status == self::STATUS_UNPAID)
+            $serie = 'UNPAID-';
+        else
+            $serie = 'REFUND-';
+
+        $this->db->select_max('nr');
+        $this->db->where('status', $status);
+        $res1 = $this->db->get('invoice');
+
+        $result = $res1->row();
+
+        $next_nr = $result->nr +1;
+        if($type == 'serie')
+            return $serie;
+        else
+            return $next_nr;
+    }
+
+    public function getInvoiceFormatedNumber($data = array())
+    {
+        $this->db->where('id', $data['id']);
+        $query = $this->db->get('invoice');
+        $result = $query->row();
+
+        $nr = str_repeat('0',4-strlen($result->nr));
+
+        return $result->serie.$nr.$result->nr;
+    }
+
+    public function getOrderNextGroupId()
+    {
+        $this->db->select_max('group_id');
+        $res1 = $this->db->get('orders');
+
+        $result = 0;
+        if ($res1->num_rows() > 0)
+        {
+            $res2 = $res1->result_array();
+            $result = $res2[0]['group_id'];
+        }
+
+        $next_group_id = $result +1;
+
+        return $next_group_id;
+    }
+
+    public function get_items($invoice_id = 0) {
+        $this->db->select('invoice_item.*, orders.course_id, orders.invoice_id as unpaid_invoice_id');
+        if ($invoice_id > 0) {
+            $this->db->where('invoice_item.invoice_id', $invoice_id);
+        }
+        $this->db->join('orders', 'invoice_item.rel_id = orders.id', 'left');
+
+        return $this->db->get('invoice_item');
+    }
+
+    public function mark_as_paid($invoice_id = "") {
+        $model = $this->get_invoice($invoice_id)->row();
+        $items = $this->get_items($invoice_id);
+
+        $data = [
+            'serie' => $this->getInvoiceNumber(self::STATUS_PAID, 'serie'),
+            'nr' => $this->getInvoiceNumber(self::STATUS_PAID, 'nr'),
+            'status' => self::STATUS_PAID,
+            'paid_at' => time(),
+            'last_modified' => time(),
+        ];
+        $this->db->where('id', $invoice_id);
+        $this->db->update('invoice', $data);
+
+        if ($items->num_rows() > 0) {
+            foreach ($items->result_array() as $i => $item) {
+                $data2['user_id'] = $model->user_id;
+                $data2['course_id'] = $item['course_id'];
+                $data2['date_added'] = strtotime(date('D, d-M-Y'));
+                $this->db->insert('enroll', $data2);
+
+                $this->order_model->activate($item['rel_id']);
+            }
+        }
+
+        $this->session->set_flashdata('flash_message', get_phrase('invoice_marked_as_paid_successfully'));
+    }
+}
---
+++ application/models/Order_model.php
@@ -0,0 +1,134 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Order_model extends CI_Model {
+
+    const PAYMENT_TYPE_CASH = 1;
+    const PAYMENT_TYPE_CREDIT = 2;
+
+    const STATUS_PENDING = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_SUSPENDED = 2;
+    const STATUS_CANCELED = 3;
+
+    function __construct()
+    {
+        parent::__construct();
+        /*cache control*/
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+        $this->output->set_header('Pragma: no-cache');
+    }
+
+    public function get_order($id = 0) {
+        if ($id > 0) {
+            $this->db->where('id', $id);
+        }
+
+        return $this->db->get('orders');
+    }
+
+    public function get_all_order($id = 0) {
+        if ($id > 0) {
+            $this->db->where('id', $id);
+        }
+
+        return $this->db->get('orders');
+    }
+
+    public function add_order() {
+        $data['user_id'] = $this->input->post('user_id');
+        $data['course_id'] = $this->input->post('course_id');
+        $data['promo_id'] = $this->input->post('promo_id');
+        $data['group_id'] = $this->input->post('group_id');
+        $data['group_master'] = $this->input->post('group_master');
+        $data['title'] = html_escape($this->input->post('title'));
+        $data['invoice_id'] = $this->input->post('invoice_id');
+        $data['quantity'] = $this->input->post('quantity');
+        $data['price'] = $this->input->post('price');
+        $data['discount'] = $this->input->post('discount');
+        $data['currency_id'] = $this->input->post('currency_id');
+        $data['change_value'] = $this->input->post('change_value');
+        $data['type'] = self::PAYMENT_TYPE_CASH;
+        $data['status'] = 1;
+        $data['date_added'] = strtotime(date("Y-m-d H:i:s"));
+        return $data;
+
+        $this->db->insert('orders', $data);
+        $order_id = $this->db->insert_id();
+        $this->session->set_flashdata('flash_message', get_phrase('order_added_successfully'));
+    }
+
+    public function edit_order($order_id = "") { // Admin does this editing
+        $data = [];
+        $this->db->where('id', $order_id);
+        $this->db->update('orders', $data);
+        $this->session->set_flashdata('flash_message', get_phrase('order_update_successfully'));
+    }
+
+    public function delete_order($order_id = "") {
+        $this->db->where('id', $order_id);
+        $this->db->delete('orders');
+        $this->session->set_flashdata('flash_message', get_phrase('order_deleted_successfully'));
+    }
+
+    public function getNextGroupId()
+    {
+        $this->db->select_max('group_id');
+        $res1 = $this->db->get('orders');
+
+        $result = 0;
+        if ($res1->num_rows() > 0)
+        {
+            $res2 = $res1->result_array();
+            $result = $res2[0]['group_id'];
+        }
+
+        $next_group_id = $result +1;
+
+        return $next_group_id;
+    }
+
+    public function activate($order_id)
+    {
+        $model = $this->get_order($order_id)->row();
+
+        $data1 = [
+            'status' => self::STATUS_ACTIVE,
+            'last_modified' => time(),
+            'activated_at' => time(),
+        ];
+        $this->db->where('id', $order_id);
+        $this->db->update('orders', $data1);
+
+        // purchase course
+        $is_enrolled = $this->crud_model->is_already_enrolled($model->course_id, $model->user_id);
+        if (!$is_enrolled) {
+            $data['user_id'] = $model->user_id;
+            $data['payment_type'] = 'transferbank';
+            $data['course_id'] = $model->course_id;
+            $course_details = $this->crud_model->get_course_by_id($model->course_id)->row_array();
+            if ($course_details['discount_flag'] == 1) {
+                $data['amount'] = $course_details['discounted_price'];
+            }else {
+                $data['amount'] = $course_details['price'];
+            }
+            if (get_user_role('role_id', $course_details['user_id']) == 1) {
+                $data['admin_revenue'] = $data['amount'];
+                $data['instructor_revenue'] = 0;
+                $data['instructor_payment_status'] = 1;
+            }else {
+                if (get_settings('allow_instructor') == 1) {
+                    $instructor_revenue_percentage = get_settings('instructor_revenue');
+                    $data['instructor_revenue'] = ceil(($data['amount'] * $instructor_revenue_percentage) / 100);
+                    $data['admin_revenue'] = $data['amount'] - $data['instructor_revenue'];
+                }else {
+                    $data['instructor_revenue'] = 0;
+                    $data['admin_revenue'] = $data['amount'];
+                }
+                $data['instructor_payment_status'] = 0;
+            }
+            $data['date_added'] = strtotime(date('D, d-M-Y'));
+            $this->db->insert('payment', $data);
+        }
+    }
+}
